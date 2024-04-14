35a8219c24babb9428b22a94803e55894cd65cd3
Tgotp <435822082@qq.com>
2024-04-14 10:18:33 +0800

    123

---
+++ src/energy.h
@@ -0,0 +1,14 @@
+#include <cstdio>
+#include <iostream> 
+
+#include <iomanip>
+#include <opencv2/core/core.hpp> 
+#include <opencv2/highgui/highgui.hpp> 
+#include "lsd.h"
+using namespace std;
+using namespace cv;
+
+namespace energy
+{
+    void line_img(Mat img);
+}
